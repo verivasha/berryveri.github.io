8cce3a688ef1496bba7e36e01b7d3342614100b1
verivasha <thisiskaka992@gmail.com>
2025-04-02 21:46:34 +0500

    2

---
+++ script.js
@@ -0,0 +1,14 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.section');
+navLinks.forEach(link => {
+   link.addEventListener('click', () => {
+        const targetId = link.getAttribute('href').substring(1);
+        sections.forEach(section => {
+            if (section.id === targetId) {
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
+        });
+    });
+});
